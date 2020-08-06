563736a58e26ffd9c4f8b8a57b75b3cf6ed17e05
M.Reza Khorasany <m.reza.khorasany@gmail.com>
2020-08-06 19:49:24 +0200

    Initial commit!

--- DSS/src/hu/ak_academy/dss/DSS.java
+++ DSS/src/hu/ak_academy/dss/DSS.java
@@ -1,7 +1,9 @@
-package hu.ak_academy.dds;
+package hu.ak_academy.dss;
 
 public class DSS {
+
 	public static void main(String[] args) {
-		System.out.println("Hello DSS!");
+		System.out.println("Hello world!");
 	}
+
 }
